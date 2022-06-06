a7e62f72dbd8f0b5e36c68c61a135c2518cec309
violanerd <madalyne.cross@gmail.com>
2022-06-06 14:25:50 -0500

    check code end of 3.2

--- assets/js/game.js
+++ assets/js/game.js
@@ -29,7 +29,7 @@ var fight = function (enemyName) {
         // if player chooses to skip, confirm and then stop the loop
         if (promptFight === "skip" || promptFight === "SKIP") {
         // confirm player wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
 
             // if yes (true), leave fight
             if (confirmSkip) {
