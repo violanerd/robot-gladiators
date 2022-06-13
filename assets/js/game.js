b767675a668afda54dbf273c02fe89a4e98abb0a
violanerd <madalyne.cross@gmail.com>
2022-06-12 20:01:28 -0500

    added highscore to local storage

--- assets/js/game.js
+++ assets/js/game.js
@@ -134,12 +134,21 @@ var startGame = function() {
 //function to end the entire game
 var endGame = function() {
     window.alert("The game has now ended. Let's see how you did!");
+    
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null){
+        highScore = 0;
+    } 
     //if player is still alive, player wins!
-    if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + ".");
-    } else {
-        window.alert("You've lost your robot in battle.");
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("highscorer", playerInfo.name);
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } 
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
     }
+
 
     var playAgainConfirm = window.confirm("Would you like to play again?");
 
